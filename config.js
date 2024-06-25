f864adfbb891277baf39c8c14c7e6c5ba4b04e0d
kenyafisi <nmakuthi9@gmail.com>
2024-06-26 01:02:41 +0300

    Update config.js

--- config.js
+++ config.js
@@ -62,7 +62,8 @@ global.APIKeys = {
 }
 
 // Sticker WM
-global.botname = '' process.env.BOTNAME
+global.botname = ''
+    process.env.BOTNAME || "nicholasBot"
 global.premium = 'true'
 global.packname = 'GURU┃ᴮᴼᵀ'
 global.author = 'ᴳᵘʳᵘ ˢᵉⁿˢᵉⁱ'
